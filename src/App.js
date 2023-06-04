20bc5bfb88e5f3538d67baf274fe468090e8fb9c
Rodrigo <rodrigonottoboni@gmail.com>
2023-06-04 12:55:06 -0300

    created project using react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import logo from './imagens/logo.svg';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+       <div className='logo'>
+          <img src={logo} alt='logo'></img>
+          <p><strong>Dedé books</strong></p>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
